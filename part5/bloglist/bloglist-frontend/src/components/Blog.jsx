d8537368f10e6998749415f4132f74a3c9ca7177
Gavin Cartier <48884797+18Planet@users.noreply.github.com>
2025-01-19 13:36:34 -0800

    blog.jsx

--- part5/bloglist/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,40 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false)
+
+  const showWhenVisible = {display : visible ? "" : "none"}
+  const hideWhenVisible = {display : visible ? "none" : ""}
+
+  const changeVisibility = () => {
+    setVisible(!visible)
+  }
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  useEffect(() => {
+    
+  })
+
+  return (
+    <div style = {blogStyle}>
+      <div style={hideWhenVisible}>
+        {blog.title} by {blog.author} <button onClick = {changeVisibility}>View</button>
+      </div>
+      <div style = {showWhenVisible}>
+        {blog.title} by {blog.author} <button onClick = {changeVisibility}>Hide</button><br></br>
+        URL: {blog.url}<br></br>
+        Likes: {blog.likes} <button>Like</button><br></br> 
+      </div>
+    </div>  
+  )
+}
 
 export default Blog
